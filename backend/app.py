c8b409ce9d7e7c6284024d7ea2063bc888e8373b
mikeAbundes23 <irvingmiguel.abundes@ciencias.unam.mx>
2024-04-18 12:48:48 -0600

    se agrega dotenv

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'aksdjflka'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
 
