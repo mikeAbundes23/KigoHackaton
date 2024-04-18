daec5f4fc943e36dde28ac311f136c3d729601a8
mikeAbundes23 <irvingmiguel.abundes@ciencias.unam.mx>
2024-04-18 12:39:49 -0600

    conexion lista

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'aksdjflka'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db=SQLAlchemy(app)
+
 @app.route('/')
 def hello_world():
     return 'Hello world!'
