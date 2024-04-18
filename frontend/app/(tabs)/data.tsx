28cefc00b2141fde2f525714214f2a6051c700fc
Ian Vega <lan@ciencias.unam.mx>
2024-04-18 12:19:47 -0600

    Add new files and folders for frontend development

---
+++ frontend/app/(tabs)/data.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+
+const Data = () => {
+  return (
+    <View>
+      <StatusBar style="dark" />
+
+      <Text>Hola</Text>
+    </View>
+  );
+};
+
+export default Data;
---
+++ frontend/app/(tabs)/home.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+
+const Home = () => {
+  return (
+    <View>
+        <StatusBar style="dark" />
+      <Text>home</Text>
+    </View>
+  );
+};
+
+export default Home;
